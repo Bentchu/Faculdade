ceb237be1b09b08012cce1a9d519619af63e0f39
Bentchu <31457200+Bentchu@users.noreply.github.com>
2022-05-03 22:22:12 -0300

    Add files via upload

---
+++ Exercícios2_5.py
@@ -0,0 +1,35 @@
+#Exercício 2.5
+#Comece executando a atribuição:
+#s = '0123456789'
+#Agora, escreva expressões usando a string s e o operador de indexação que é avaliado como:
+#(a) '0'
+#(b) '1'
+#(c) '6'
+#(d) '8'
+#(e) '9'
+
+s = '0123456789'
+
+print (s[0])
+print (s[1])
+print (s[6])
+print (s[8])
+print (s[9])
+
+soma = s[0] + s[6]
+print (soma)
+#nao funciona com string subtrai = s[8] - s[6]
+#print (subtrai)
+multi = s[1] * 3
+print (multi)
+#nao funciona com string divide = s[9] / s[1]
+#print (divide)
+#nao funciona com string divide2 = s[9] // s[6]
+#print (divide2)
+#nao funciona com string potencia = s[6] ** s[8]
+#print (potencia)
+
+
+
+
+
