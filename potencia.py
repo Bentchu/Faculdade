109f7169a4f74957cb523c61c1665746e10a48ac
Bentchu <31457200+Bentchu@users.noreply.github.com>
2022-05-03 21:13:15 -0300

    Add files via upload

---
+++ potencia.py
@@ -0,0 +1,9 @@
+def potencia(base,expoente): 
+       resultado = 1 
+       for numero in range(1,expoente+1): 
+           resultado = resultado * base 
+       return resultado 
+
+numero = eval(input("Entre um número que quer calcular (base): ")) 
+expoente = eval(input("Entre o expoente: ")) 
+print( 'Potencia : ' ,potencia(numero,expoente)) 
